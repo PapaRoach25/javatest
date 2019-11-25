262695ae86030828ce1c9ad22ac463a4b44931de
PapaRoach25 <bobmarleymetal1996@gmail.com>
2019-11-25 11:26:07 +0200

    customer fixes

--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,20 @@
 import java.util.Date;
 
 public class Student {
-    private String Name;
-    private int Age;
-    private Date BirthDate;
+    private String StudentName;
+    private int StudentAge;
+    private Date StudentBirthDate;
 
     public Student(String name, int age, Date asdasd){
-            this.Name = name;
-            this.Age = age;
-        this.BirthDate =asdasd;
+            this.StudentName = name;
+            this.StudentAge = age;
+        this.StudentBirthDate =asdasd;
     }
-
-
-
-
+    public boolean IsStudentTest(){
+        if(true)
+            return true;
+        return false;
+    }
     public void SayHello(){
         System.out.println("Hello");
     }
