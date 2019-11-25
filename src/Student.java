5f465e1676085d2dddd078bdf961b3adce706ffb
PapaRoach25 <bobmarleymetal1996@gmail.com>
2019-11-25 11:17:27 +0200

    added constructor

--- src/Student.java
+++ src/Student.java
@@ -4,4 +4,10 @@ public class Student {
     private String Name;
     private int Age;
     private Date BirthDate;
+
+    public Student(String name, int age, Date asdasd){
+            this.Name = name;
+            this.Age = age;
+        this.BirthDate =asdasd;
+    }
 }
